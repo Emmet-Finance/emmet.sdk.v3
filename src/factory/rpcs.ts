6d1e4f0ab14aceb5372a88b6df84d61e0d209cc3
Rishabh Kumar <rsbkme@gmail.com>
2024-08-12 01:52:22 +0530

    Add: rpcs

--- src/factory/rpcs.ts
+++ src/factory/rpcs.ts
@@ -1,7 +1,27 @@
 export const TestNetRpcUri = {
-  BSC: ["https://bsc-testnet.blockpi.network/v1/rpc/public"],
-  ETH: ["https://eth-sepolia.public.blastapi.io"],
-  POLYGON: ["https://rpc-amoy.polygon.technology/"],
-  BERACHAIN: ["https://bartio.rpc.berachain.com"],
+  BSC: [
+    "https://bsc-testnet.blockpi.network/v1/rpc/public",
+    "https://api.zan.top/node/v1/bsc/testnet/public",
+    "https://bsc-testnet.public.blastapi.io",
+    "https://bsc-testnet-rpc.publicnode.com",
+  ],
+  ETH: [
+    "https://eth-sepolia.public.blastapi.io",
+    "https://eth-sepolia.api.onfinality.io/public",
+    "https://gateway.tenderly.co/public/sepolia",
+    "https://ethereum-sepolia-rpc.publicnode.com",
+  ],
+  POLYGON: [
+    "https://rpc-amoy.polygon.technology/",
+    "https://rpc.ankr.com/polygon_amoy",
+    "https://polygon-amoy.gateway.tatum.io",
+    "https://polygon-amoy-bor-rpc.publicnode.com",
+  ],
+  BERACHAIN: [
+    "https://bartio.rpc.berachain.com",
+    "https://bera-testnet.nodeinfra.com",
+    "https://bartio.drpc.org",
+    "https://bartio.rpc.b-harvest.io",
+  ],
   ONLYLAYER: ["https://onlylayer.org"],
 } as const;
