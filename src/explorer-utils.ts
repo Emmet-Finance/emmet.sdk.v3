ec41c0d2ea3782c8a7306865289d0ba268f4bb19
Mithras <dimabrook79@gmail.com>
2024-10-10 20:47:50 +0300

    add: chainToDomain

--- src/explorer-utils.ts
+++ src/explorer-utils.ts
@@ -1,6 +1,16 @@
 import { Chain } from "./factory/types";
 
 export const ChainIDToDomain = {
+  // Mainnets
+  1: Chain.ETHEREUM,
+  43114: Chain.AVALANCHE,
+  10: Chain.OPTIMISM,
+  42161: Chain.ARBITRUM,
+  56: Chain.BSC,
+  8453: Chain.BASE,
+  137: Chain.POLYGON,
+  65534: Chain.TON,
+  // Testnets
   80002: Chain.POLYGON,
   97: Chain.BSC,
   11155111: Chain.ETHEREUM,
