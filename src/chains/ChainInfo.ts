eece92045e23111fda43225f16f60dd0ad057c2e
Mithras <dimabrook79@gmail.com>
2024-10-12 21:43:59 +0300

    add: CHAIN_INFO file

---
+++ src/chains/ChainInfo.ts
@@ -0,0 +1,75 @@
+import { Chain, ChainInfo } from "../factory/types";
+import { tonHandler } from "./ton";
+import { web3Helper } from "./web3";
+
+export const CHAIN_INFO: ChainInfo = new Map();
+
+CHAIN_INFO.set(Chain.ARBITRUM, {
+  constructor: web3Helper,
+  decimals: 18,
+  name: "Arbitrum",
+  nonce: Chain.ARBITRUM
+});
+
+CHAIN_INFO.set(Chain.AVALANCHE, {
+  constructor: web3Helper,
+  decimals: 18,
+  name: "Avalanche",
+  nonce: Chain.AVALANCHE
+});
+
+CHAIN_INFO.set(Chain.BERACHAIN, {
+  constructor: web3Helper,
+  decimals: 18,
+  name: "Berachain",
+  nonce: Chain.BERACHAIN,
+});
+
+CHAIN_INFO.set(Chain.BASE, {
+  constructor: web3Helper,
+  decimals: 18,
+  name: "BASE",
+  nonce: Chain.BASE
+});
+
+CHAIN_INFO.set(Chain.BSC, {
+  constructor: web3Helper,
+  decimals: 18,
+  name: "BSC",
+  nonce: Chain.BSC,
+});
+
+CHAIN_INFO.set(Chain.ETHEREUM, {
+  constructor: web3Helper,
+  decimals: 18,
+  name: "Ethereum",
+  nonce: Chain.ETHEREUM,
+});
+
+CHAIN_INFO.set(Chain.ONLYLAYER, {
+  constructor: web3Helper,
+  decimals: 18,
+  name: "Only Layer",
+  nonce: Chain.ONLYLAYER,
+});
+
+CHAIN_INFO.set(Chain.OPTIMISM, {
+  constructor: web3Helper,
+  decimals: 18,
+  name: "Optimism",
+  nonce: Chain.OPTIMISM
+});
+
+CHAIN_INFO.set(Chain.POLYGON, {
+  constructor: web3Helper,
+  decimals: 18,
+  name: "Polygon",
+  nonce: Chain.POLYGON,
+});
+
+CHAIN_INFO.set(Chain.TON, {
+  decimals: 18,
+  name: "Ton",
+  nonce: Chain.TON,
+  constructor: async (...args) => tonHandler(...args),
+});
