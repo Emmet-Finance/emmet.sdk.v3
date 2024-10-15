7c4fc7848679c81d03b8f8ce63a47c1568f5d1de
Mithreum <120666231+Mithreum@users.noreply.github.com>
2024-10-15 15:24:59 +0000

    Build: (e92e56c) fix: parseCallData returns ReceiveParams

--- dist/chains/index.d.ts
+++ dist/chains/index.d.ts
@@ -2,13 +2,14 @@ import { BigNumberish } from "ethers";
 import type { ChainNonce } from "../factory/types";
 import { CrossChainTransaction } from "@emmet-contracts/web3/dist/contracts/consensus/Consensus";
 export type ReceiveParams = {
-    ccmHash: string;
     blockNumber: BigNumberish;
     foreignIndexOut: BigNumberish;
+    value: BigNumberish;
+    timestamp: BigNumberish;
     sentAmount: BigNumberish;
     receiveAmount: BigNumberish;
-    fromChainId: number;
-    toChainId: number;
+    fromChainId: BigNumberish;
+    toChainId: BigNumberish;
     to: string;
     fromToken: string;
     toToken: string;
