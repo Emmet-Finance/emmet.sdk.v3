a89a4a153d1bb862dece4a52b855aaacfe5f7533
Mithreum <120666231+Mithreum@users.noreply.github.com>
2024-10-22 19:16:45 +0000

    Build: (7b3bed1) fix: ton coin & token balances

--- dist/chains/ton.d.ts
+++ dist/chains/ton.d.ts
@@ -24,6 +24,12 @@ export interface TonParams {
     stonRouterAddress: string;
     pTonAddress: string;
 }
+/**
+ *
+ * @param ms number of milliseconds to wait
+ * @returns halts the program execution for the `ms` milliseconds
+ */
+export declare const sleep: (ms: number) => Promise<unknown>;
 export declare function tonHandler({ rpcs, nativeTokenId, chainName, chainId, stonApiUrl, addressBook, stonRouterAddress, pTonAddress, }: TonParams): Promise<TonHelper>;
 export declare function raise(msg: string): never;
 export declare function assertNotNull<T>(t: T | null | undefined): t is T;
