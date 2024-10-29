6578a186d9b9706b6c7590348603eafd431bf034
Mithreum <120666231+Mithreum@users.noreply.github.com>
2024-10-29 14:22:39 +0000

    Build: (95099de) add: more Consensus functions

--- dist/chains/getConsensus.d.ts
+++ dist/chains/getConsensus.d.ts
@@ -17,7 +17,37 @@ export declare function getConsensus(): Promise<Consensus>;
  * @returns ReceiveParams
  */
 export declare function decodeCalldata(calldata: string): ReceiveParams | undefined;
+/**
+ * Deserialized a CCM transaction
+ * @param tx a CCM transaction from the Consensus
+ * @returns a deserialized CCM transaction
+ */
 export declare function decodeCCMTransaction(tx: CrossChainTransaction.CCTStructOutput): CrossChainTransaction.CCTStructOutput;
+/**
+ * Fetches a CCM `txHash` by the original chain hash
+ * @param consensus a consensus instance
+ * @param originalHash the original chain hash
+ * @param batch batch of searched txs
+ * @returns a CCM hash
+ */
 export declare function ccmHashByOriginalHash(consensus: Consensus, originalHash: string, batch?: BigNumberish): Promise<string>;
-export declare function getTransaction(consensus: Consensus, ccmHash: string): Promise<CrossChainTransaction.CCTStructOutput>;
+/**
+ * Fetches a single CCM Transaction
+ * @param consensus the consensus instance
+ * @param ccmHash a Cross-Chain Messaging ID
+ * @returns a CCM txHash
+ */
+export declare function getTransaction(consensus: Consensus, ccmHash: string): Promise<CrossChainTransaction.CCTStructOutput & {
+    decoded?: ReceiveParams;
+}>;
+/**
+ * Fetches the `batch` of transactions
+ * @param consensus the consensus instance
+ * @param batch batch of searched txs
+ * @param skip the number of ommited txs
+ * @returns the `batch` or an available number of txs
+ */
+export declare function getTransactions(consensus: Consensus, batch?: BigNumberish, skip?: BigNumberish): Promise<(CrossChainTransaction.CCTStructOutput & {
+    decoded?: any;
+})[]>;
 //# sourceMappingURL=getConsensus.d.ts.map
