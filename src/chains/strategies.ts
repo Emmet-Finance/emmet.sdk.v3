b7bc8a8ce9c619fb14a0874fef1d61764f9e9085
Mithras <dimabrook79@gmail.com>
2025-01-26 14:53:34 +0200

    add: strategies

---
+++ src/chains/strategies.ts
@@ -0,0 +1,17 @@
+export namespace TonStrategies {
+    export const NOTHING            = 0n;
+    export const CCTP_BURN          = 1n;
+    export const CCTP_CLAIM         = 2n;
+    export const LOCK               = 3n;
+    export const MINT               = 4n;
+    export const BURN               = 5n;
+    export const UNLOCK             = 6n;
+    export const PASS_TO_LP         = 7n;
+    export const TRANSFER_FROM_LP   = 8n;
+    export const SWAP_1             = 9n;
+    export const SWAP_2             = 10n;
+    export const SWAP_3             = 11n;
+    export const SWAP_4             = 12n;
+    export const SWAP_5             = 13n;
+    export const SWAP_6             = 14n;
+}
